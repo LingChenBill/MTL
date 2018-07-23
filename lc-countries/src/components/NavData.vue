9277a5cd5b5698050f56a3af5cbbdf7b77f2158d
zhuyz <lingchen1316@163.com>
2018-07-23 23:53:39 +0800

    NavData create

---
+++ lc-countries/src/components/NavData.vue
@@ -0,0 +1,108 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column
+      prop="nation"
+      label="国藉"
+      sortable
+      width="180"
+      :filters="[{text: '中国', value: '中国'}, {text: '美国', value: '美国'}, {text: '韩国', value: '韩国'}, {text: '日本', value: '日本'}]"
+      :filter-method="filterHandler">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="日期"
+      sortable
+      width="180"
+      :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+      :filter-method="filterHandler">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址"
+      :formatter="formatter">
+    </el-table-column>
+    <el-table-column
+      prop="tag"
+      label="标签"
+      width="100"
+      :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+      :filter-method="filterTag"
+      filter-placement="bottom-end">
+      <template slot-scope="scope">
+        <el-tag
+          :type="scope.row.tag ==='家' ? 'primary' : 'success'"
+          disable-transitions>
+          {{ scope.row.tag }}
+        </el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tableData: [{
+        nation: '中国',
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        tag: '家'
+      }, {
+        nation: '美国',
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        tag: '公司'
+      }, {
+        nation: '韩国',
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        tag: '家'
+      }, {
+        nation: '日本',
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        tag: '公司'
+      }]
+    }
+  },
+  methods: {
+    formatter (row, column) {
+      return row.address
+    },
+    filterTag (value, row) {
+      return row.tag === value
+    },
+    filterHandler (value, row, column) {
+      const property = column['property']
+      return row[property] === value
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
